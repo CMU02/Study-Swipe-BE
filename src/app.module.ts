106bec4b0c8c94af630f7f3f60effe0715fc9cc4
HyeonJun <hyeonjun1968@naver.com>
2025-09-01 23:30:30 +0900

    feat: 인증관련 모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from './mailer/mailer.module';
+import { UserModule } from './user/user.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { TagsModule } from './tags/tags.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE || 'study_swipe',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    MailerModule,
+    UserModule,
+    ProfilesModule,
+    TagsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

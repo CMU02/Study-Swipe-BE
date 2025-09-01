99b22b148352f278a24957ff06960b4bad2b0906
HyeonJun <hyeonjun1968@naver.com>
2025-09-01 23:29:15 +0900

    feat: userService 설정 - TODO: 추후 관련 로직 추가 예정

---
+++ src/user/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+    constructor(
+        private userRepository: Repository<User>
+    ) {}
+}

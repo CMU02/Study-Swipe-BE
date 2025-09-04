2f0a413fbf2afac4a32825bec916a2937742a920
CMU02 <Hyeonjun1968@naver.com>
2025-09-04 20:08:49 +0900

    feat: 성공시 반환하는 기본구조 추가

--- src/base_response.ts
+++ src/base_response.ts
@@ -3,4 +3,11 @@ import { HttpStatus } from "@nestjs/common";
 export interface BaseResponse {
     status_code: HttpStatus;
     message: string;
+    option?: {
+        data?: {
+            accessToken: string
+            refreshToken: string
+        },
+        meta_data?: Record<string, unknown>;
+    }
 }

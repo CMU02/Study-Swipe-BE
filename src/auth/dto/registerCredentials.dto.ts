afcc1e0336b6d12d50fb892a3a0c81ba799f7f32
CMU02 <Hyeonjun1968@naver.com>
2025-09-04 20:07:28 +0900

    refactor: signup dto 이름 수정
    - registerCredentials으로 수정

--- src/auth/dto/registerCredentials.dto.ts
+++ src/auth/dto/registerCredentials.dto.ts
@@ -1,9 +1,9 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
-export class SignUpDto {
+export class RegisterCredentialsDto {
     @IsString()
     user_id: string;
-    
+
     @IsString()
     @MinLength(8, { message : '비밀번호는 최소 8자 이상이어야 합니다.'})
     user_pwd: string;

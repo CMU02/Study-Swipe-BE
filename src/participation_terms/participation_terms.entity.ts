bcf99161a13a8d10c84bd3cdc3bb8a24220ef2cd
HyeonJun <hyeonjun1968@naver.com>
2025-09-07 22:14:24 +0900

    modify: 사교모임 가능여부 및 참여기간 엔티티의 기본키 자동생성으로 변경 feat: 변경함수들에 대한 인터페이스

--- src/participation_terms/participation_terms.entity.ts
+++ src/participation_terms/participation_terms.entity.ts
@@ -1,14 +1,14 @@
-import { Profiles } from "src/profiles/profiles.entity";
-import { Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { Profiles } from 'src/profiles/profiles.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class ParticipationTerms {
-  @PrimaryColumn({ name: 'participation_term_id'})
-  id: string
+  @PrimaryGeneratedColumn('increment', { name: 'participation_term_id' })
+  id: string;
 
   @Column()
-  name: string
+  name: string;
 
   @OneToMany(() => Profiles, (profiles) => profiles.participation_terms)
-  profiles: Profiles
+  profiles: Profiles;
 }
--- src/social_prefs/social_prefs.entity.ts
+++ src/social_prefs/social_prefs.entity.ts
@@ -1,14 +1,14 @@
 import { Profiles } from 'src/profiles/profiles.entity';
-import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class SocialPrefs {
-  @PrimaryColumn({ name: 'social_pref_id' })
+  @PrimaryGeneratedColumn('increment', { name: 'social_pref_id' })
   id: string;
 
   @Column()
   name: string;
 
   @OneToMany(() => Profiles, (profiles) => profiles.social_pref)
-  profiles: Profiles
+  profiles: Profiles;
 }

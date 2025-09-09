dd138e68032e48806e33471e374ebcee1e580551
HyeonJun <hyeonjun1968@naver.com>
2025-09-09 17:33:46 +0900

    refactor: 파일 명명 규칙 수정

--- src/smoking_status/smoking_status.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SmokingStatus } from './smoking_status.entity';
-
-describe('SmokingStatus', () => {
-  it('should be defined', () => {
-    expect(new SmokingStatus()).toBeDefined();
-  });
-});
